17302d71a5862a326d3e34079e43723dc6ba1ae1
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2022-02-18 00:12:20 +0300

    feat(state): add paused to timer state

--- src/state.rs
+++ src/state.rs
@@ -13,15 +13,27 @@ pub struct App {
     pub notifier: Timer,
 }
 
+impl App {
+    pub fn new(sound_sender: sound::Sender, paused: bool) -> Self {
+        Self {
+            paused,
+            micro_break: BreakTimer::new(paused),
+            rest_break: BreakTimer::new(paused),
+            notifier: Timer::new(false),
+            sound_sender: Rc::new(sound_sender),
+        }
+    }
+}
+
 #[derive(Clone, Data, Lens)]
 pub struct BreakTimer {
     pub work_timer: Timer,
 }
 
 impl BreakTimer {
-    pub fn new() -> Self {
+    pub fn new(paused: bool) -> Self {
         Self {
-            work_timer: Timer::new(),
+            work_timer: Timer::new(paused),
         }
     }
 }
@@ -30,13 +42,15 @@ impl BreakTimer {
 pub struct Timer {
     pub progress: f64,
     pub time: String,
+    pub paused: bool,
 }
 
 impl Timer {
-    pub fn new() -> Self {
+    pub fn new(paused: bool) -> Self {
         Self {
             progress: Default::default(),
             time: Default::default(),
+            paused,
         }
     }
 
